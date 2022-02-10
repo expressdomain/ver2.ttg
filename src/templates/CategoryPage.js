cf149ecb0dad86c5071cb8795765d91e15c1c3ce
steelbridge-io <chris@steelbridge.io>
2022-02-09 16:30:17 -0800

    Changed out banner

--- src/templates/CategoryPage.js
+++ src/templates/CategoryPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Link, graphql} from "gatsby";
 import Footer from "@/components/footer";
 import Layout from "@/components/layout";
-import PageBanner from "@/components/page-banner";
+import PageBanner from "@/components/category-banner";
 import MenuContextProvider from "@/context/menu-context";
 import SearchContextProvider from "@/context/search-context";
 import HeaderOne from "@/components/header-one";
@@ -19,37 +19,28 @@ const TrinityGuideCatPage = ({pageContext, data}) => (
 		<SearchContextProvider>
 			<Layout PageTitle="Services Page">
 				<HeaderOne />
-				<PageBanner title={pageContext.title} name={pageContext.title} />
+				<PageBanner title={pageContext.category} name={pageContext.category} />
 				<section className="commonSection">
-					<CategoriesList />
 					<div className="page-wrapper">
 					
-					
-					
 					<div className="post-list-wrapper">
-						<h1>{pageContext.category}</h1>
 						<ul>
 							{data.allWpPost.edges.map((post, key) => (
-								
 								<li key={key}>
 									<Link to={`/${post.node.slug}`}>
 										<h3 className="post-title" dangerouslySetInnerHTML={{__html: post.node.title}} />
 									</Link>
+									<small dangerouslySetInnerHTML={{__html: post.node.date}} />
 										<div className='page-container-content' dangerouslySetInnerHTML={{__html: post.node.excerpt}} />
-									
-									
 								</li>
-								
 							))}
-							
-							
 						</ul>
 					</div>
 					</div>
 					
-					
-					
-					
+				</section>
+				<section className="commonSection">
+					<CategoriesList />
 				</section>
 				<Footer />
 			</Layout>
