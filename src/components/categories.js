a0d185a727d1fc8465ffdca559662a5b827f68f8
steelbridge-io <chris@steelbridge.io>
2022-02-09 16:28:37 -0800

    Layout adjustment

--- src/components/categories.js
+++ src/components/categories.js
@@ -22,24 +22,20 @@ export const CategoriesList = () => {
 	const { edges } = useGetCategories()
 	
 	return (
-		<div className="categories">
-			<ul>
-				<li>
-					<Link to="/">
-						All Posts
-					</Link>
-				</li>
+		<div className="container-fluid">
+			<div className="row justify-content-center flex-row">
 					{
 						edges.map((category, key) => (
-							<li key={`key`}>
-								<Link to={`/category/${category.node.slug}`}>
+							
+							<div className="ml-1 mr-1 mt-1 mb-1 col-lg-1 col-md-2 col-sm-3 col-xs-6 category-chiclet align-items-center justify-content-center d-flex" key={`key`}>
+								<Link className="d-flex justify-content-center align-items-center" to={`/category/${category.node.slug}`}>
 									{category.node.name}
 								</Link>
-							</li>
+							</div>
+						
 						))
 					}
-				
-			</ul>
+			</div>
 		</div>
 	)
 }
