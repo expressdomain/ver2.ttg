31dd2d9718fe43763bd84974b852752074781d1f
steelbridge-io <chris@steelbridge.io>
2022-02-12 00:50:36 -0800

    added gatsby-plugin-wpgraphql-seo

--- src/templates/report-archive.js
+++ src/templates/report-archive.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Seo from 'gatsby-plugin-wpgraphql-seo';
 import Footer from "@/components/footer";
 import Layout from "@/components/layout";
 import PageBanner from "@/components/page-banner";
@@ -11,6 +12,7 @@ import "../assets/scss/wordpress-styles.css";
 import "@wordpress/block-library/build-style/style.css";
 import "@wordpress/block-library/build-style/theme.css";
 import {CategoriesList} from "../components/categories";
+import {graphql} from "gatsby";
 
 const FeaturedImage = styled.img`
   max-width: 100%;
@@ -22,34 +24,77 @@ const FeaturedImage = styled.img`
 `
 
 
-const reportArchiveConst = ({pageContext}) => (
-	<MenuContextProvider>
-		<SearchContextProvider>
-			<Layout PageTitle="Fishing Reports - The Trinity Guide Co.">
-				<HeaderOne />
-				<PageBanner title={pageContext.title} name={pageContext.title} />
-				<section className="commonSection">
-					<div className="report-wrapper">
-					<div className='container page-container'>
-						
-						<div className="container featured-image">
-						<FeaturedImage src={pageContext.featuredImage.node.sourceUrl} />
+const reportArchiveConst = ({pageContext, data: { wpReport }}) => (
+	<>
+		<Seo post={wpReport} />
+		<MenuContextProvider>
+			<SearchContextProvider>
+				<Layout PageTitle="Fishing Reports - The Trinity Guide Co.">
+					<HeaderOne />
+					<PageBanner title={pageContext.title} name={pageContext.title} />
+					<section className="commonSection">
+						<div className="report-wrapper">
+						<div className='container page-container'>
+							
+							<div className="container featured-image">
+							<FeaturedImage src={pageContext.featuredImage.node.sourceUrl} />
+							</div>
+							
+							<div className='page-container-content' dangerouslySetInnerHTML={{__html: pageContext.content}} />
 						</div>
-						
-						<div className='page-container-content' dangerouslySetInnerHTML={{__html: pageContext.content}} />
-					</div>
-					</div>
-				</section>
-				<section className="commonSection category-chiclet-wrapper">
-					<CategoriesList />
-				</section>
-				<Footer />
-			</Layout>
-		</SearchContextProvider>
-	</MenuContextProvider>
+						</div>
+					</section>
+					<section className="commonSection category-chiclet-wrapper">
+						<CategoriesList />
+					</section>
+					<Footer />
+				</Layout>
+			</SearchContextProvider>
+		</MenuContextProvider>
+		</>
 );
 
 export default reportArchiveConst
+
+export const reportQuery = graphql`
+	query GET_REPORT_POST($id: String!) {
+        wpReport(id: { eq: $id }) {
+            nodeType
+            title
+            uri
+            seo {
+                title
+                metaDesc
+                focuskw
+                metaKeywords
+                metaRobotsNoindex
+                metaRobotsNofollow
+                opengraphTitle
+                opengraphDescription
+                opengraphImage {
+                    altText
+                    sourceUrl
+                    srcSet
+                }
+                twitterTitle
+                twitterDescription
+                twitterImage {
+                    altText
+                    sourceUrl
+                    srcSet
+                }
+                canonical
+                cornerstone
+                schema {
+                    articleType
+                    pageType
+                    raw
+                }
+            }
+        }
+    }
+`;
+
 
 
 
