42d450bbfb4f80b428ceaa073fab59613f1a1bc2
chris@steelbridge.io </*fa11r1VER/*/>
2022-02-15 14:44:41 -0800

    Added mailchimp to signup form

--- src/components/subscribe-form.js
+++ src/components/subscribe-form.js
@@ -13,18 +13,20 @@ const SubscribeForm = () => {
             <h2 className="sec_title">{sectionContent.title}</h2>
           </Col>
           <Col lg={8}>
-            <form action="" method="post" className="subscribefrom about-page-subscribe">
-              <input type="email" placeholder="Enter your email" name="email" />
+            <div id="mc_embed_signup">
+            <form action="https://gmail.us3.list-manage.com/subscribe/post?u=b61e2eb5f241dbe27bea34345&amp;id=1239cb6c1b" method="post" className="validate subscribefrom about-page-subscribe" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" novalidate>
+              <input type="email" placeholder="Enter your email here, then Sign Up >" name="EMAIL" className="email" id="mce-EMAIL" required />
               <button className="common_btn red_bg about-sub-btn" type="submit" name="submit">
-                <span>Subscribe now</span>
+                <span>Sign Up!</span>
               </button>
             </form>
+            </div>
             <div className="card subscribe-feat-list">
               <div className="card-body">
                 <ul className="list-group list-group-flush">
-                  <li className="list-group-item">Fishing Reports</li>
-                  <li className="list-group-item">Tips &amp; Tactics</li>
-                  <li className="list-group-item">Seasonal Insights &amp; Plans</li>
+                  <li className="list-group-item">We Deliver -> Fishing Reports</li>
+                  <li className="list-group-item">We Deliver -> Tips &amp; Tactics</li>
+                  <li className="list-group-item">We Deliver -> Seasonal Insights &amp; Plans</li>
                 </ul>
               </div>
             </div>
